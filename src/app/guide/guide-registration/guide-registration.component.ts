a9b40af86e03dc570a839c2a32cf3157663bddfa
devil730 <a30sham.07@gmail.com>
2023-02-10 11:48:20 +0530

    new update

---
+++ src/app/guide/guide-registration/guide-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guide-registration',
+  templateUrl: './guide-registration.component.html',
+  styleUrls: ['./guide-registration.component.css']
+})
+export class GuideRegistrationComponent {
+
+}
---
+++ src/app/guide/guide.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GuideRegistrationComponent } from './guide-registration/guide-registration.component';
+
+
+
+@NgModule({
+  declarations: [
+    GuideRegistrationComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    GuideRegistrationComponent
+  ]
+})
+export class GuideModule { }
